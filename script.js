f177cc9bdef4a140633307d4126c64e83e7cd56c
Nahid44332 <nahidhossen3278@gmail.com>
2025-02-09 19:19:09 -0800

    Class-test-3

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+let years;
+
+years = 4;
+if(years>=60){
+    console.log("Ticket price =8$");
+}
+else if(years>=18){
+    console.log("Ticket price = 15$");
+}
+else if(years>=5){
+    console.log("Ticket price = 10$");
+}
+else{
+    console.log("Ticket price = Free");
+}
